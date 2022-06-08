20e43fe6c3a14e4823f10fe4fa7913dfb5c11f04
Talita <talitazmiza@gmail.com>
2022-06-08 19:10:13 +0700

    Footer component

---
+++ sections/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="py-6 text-gray-600 text-center text-sm">
+      <span className="text-grey-900 font-bold text-lg mr-2">
+        Lita
+      </span>&copy; {new Date().getFullYear()} All Rights
+    </footer>
+  )
+}
+
+export default Footer
