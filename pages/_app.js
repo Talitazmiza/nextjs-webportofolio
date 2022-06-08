2747f2f07c49222c8001284112afdfaf707703c1
Talita <talitazmiza@gmail.com>
2022-06-08 20:10:46 +0700

    fix layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Layout from '../components/layout';
+import Layout from "../components/Layout";
 import {ThemeProvider} from "next-themes";
 
 function MyApp({ Component, pageProps }) {
